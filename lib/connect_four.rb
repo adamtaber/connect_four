7f7f52fd8c7fa0f2e477ace41601b2746396434d
Adam Taber <altaber@gmail.com>
2022-07-20 16:06:37 -0500

    add all methods and test connect_four method

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,13 +1,16 @@
 class ConnectFour
-  attr_accessor :board
+  attr_accessor :board 
+  attr_accessor :turn_number
 
   def initialize
     @board = self.create_board
     @player_input = nil
+    @turn_number = 0
+    @player_symbol = nil
   end
 
   def create_board
-    Array.new(6) { Array.new(7,"\u25ef") }
+    Array.new(6) { Array.new(7,"\u26AB") }
   end
 
   def print_board
@@ -16,28 +19,43 @@ class ConnectFour
       array.each do |item|
         print "#{item} | "
       end
-      print "\n"
-      print " --------------------------- "
-      print "\n"
+      print "\n ---------------------------------- \n"
     end
+    print "   1    2    3    4    5    6    7  \n"
   end
 
   def play_game
+    puts "Welcome to Connect Four \n"
+    print_board
     until game_over?
-      puts "Please enter a value corresponding to your chosen column"
+      if @turn_number % 2 == 0
+        puts "Please enter a value corresponding to your chosen column, Player 1"
+      elsif @turn_number % 2 == 1
+        puts "Please enter a value corresponding to your chosen column, Player 2"
+      end
       get_player_input
       if column_full?(@player_input)
         puts "That column is already full. Please choose a different column"
         get_player_input
       end
+      if @turn_number % 2 == 0
+        @player_symbol = "\u2B55"
+        place_tile(@player_input, @player_symbol)
+      elsif @turn_number % 2 == 1
+        @player_symbol = "\u26aa"
+        place_tile(@player_input, @player_symbol)
+      end
+      @turn_number += 1
+      print_board
     end
+    display_winner
   end
 
   def get_player_input
     loop do
       player_input = gets.chomp.to_i
       if player_input >= 1 && player_input <= 7
-        @player_input == player_input
+        @player_input = player_input
         return
       else
         puts 'That number is invalid. Please enter a number between 1 and 7'
@@ -45,10 +63,115 @@ class ConnectFour
     end
   end
 
-  def column_full?(input)
-    @board[0][input-1] != "\u25ef"
+  def column_full?(column)
+    @board[0][column-1] != "\u26AB"
   end
-end
 
+  def place_tile(column, player)
+    found_location = false
+    column_arr = @board.transpose[column-1].reverse
+    new_column = column_arr.map do |n|
+      if found_location == false && n == "\u26AB"
+        found_location = true
+        n = player
+      else
+        n = n
+      end
+    end
+    temp_board = @board.transpose
+    temp_board.delete_at(column-1)
+    temp_board.insert(column-1, new_column.reverse)
+    #temp_board.unshift(new_column.reverse)
+    @board = temp_board.transpose
+  end
+
+  def game_over?
+    true if connect_four?(@player_symbol) || last_turn?
+  end
+
+  def connect_four?(player)
+    true if connect_row?(player) || connect_column?(player) || connect_plus_slope?(player) || connect_neg_slope?(player)
+  end
+
+  def connect_row?(player)
+    connect_four = false
+    count = 0
+    @board.each do |arr|
+      arr.each do |n|
+        if count == 4
+          connect_four = true
+        elsif n == player
+          count += 1
+        else
+          count = 0
+        end
+      end
+    end
+    connect_four
+  end
+
+  def connect_column?(player)
+    connect_four = false
+    count = 0
+    @board.transpose.each do |arr|
+      arr.each do |n|
+        if count == 4
+          connect_four = true
+        elsif n == player
+          count += 1
+        else
+          count = 0
+        end
+      end
+    end
+    connect_four
+  end
+
+  def connect_plus_slope?(player)
+    connect_four = false
+    5.downto(3) do |x|
+      0.upto(3) do |y|
+        if @board[x][y] == @board[x-1][y+1] && @board[x][y] == @board[x-2][y+2] && @board[x][y] == @board[x-3][y+3] && @board[x][y] == player
+          connect_four = true
+        end
+      end
+    end
+    connect_four
+  end
+
+  def connect_neg_slope?(player)
+    connect_four = false
+    0.upto(2) do |x|
+      0.upto(3) do |y|
+        if @board[x][y] == @board[x+1][y+1] && @board[x][y] == @board[x+2][y+2] && @board[x][y] == @board[x+3][y+3] && @board[x][y] == player
+          connect_four = true
+        end
+      end
+    end
+    connect_four
+  end
+
+  def last_turn?
+    @turn_number == 42
+  end
+
+  def display_winner
+    if last_turn? && !connect_four?(@player_symbol)
+      puts "It's a tie!"
+    elsif (@turn_number - 1) % 2 == 0
+      puts "Player 1 is the winner!"
+    elsif (@turn_number - 1) % 2 == 1
+      puts "Player 2 is the winner!"
+    end
+  end
+
+end
+test = ConnectFour.new
+test.play_game
+
+#default symbol: "\u25ef"
 #player1 symbol: "\u26aa"
 #player2 symbol: "\u26ab"
+
+#\u2B55
+#\u2B24
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -69,6 +69,78 @@ describe ConnectFour do
       end
     end 
   end
+
+  describe '#place_tile' do
+    subject { described_class.new }
+
+    it 'places the tile in an empty column' do
+      subject.place_tile(1, "\u26aa")
+      expect(subject.board[5][0]).to eq("\u26aa")
+    end
+
+    context "when placing multiple tiles" do
+      before do
+        subject.place_tile(1, "\u26aa")
+        subject.place_tile(1, "\u2b55")
+      end
+
+      it 'shows the first tile in the lowest position' do
+        expect(subject.board[5][0]).to eq("\u26aa")
+      end
+
+      it 'shows the second tile in the second lowest position' do
+        expect(subject.board[4][0]).to eq("\u2b55")
+      end
+    end
+  end
+
+  describe '#last_turn?' do
+    subject { described_class.new }
+    it 'returns true if it is the last turn' do
+      subject.turn_number = 42
+      expect(subject.last_turn?).to be(true)
+    end
+
+    it 'returns false if it is not the last turn' do
+      subject.turn_number = 4
+      expect(subject.last_turn?).to be(false)
+    end
+  end
+
+  describe '#connect_four?' do
+    subject { described_class.new }
+    it 'returns true if 4 of the same symbol in a row' do
+      subject.board[5][0] = "\u2B55"
+      subject.board[5][1] = "\u2B55"
+      subject.board[5][2] = "\u2B55"
+      subject.board[5][3] = "\u2B55"
+      expect(subject.connect_four?("\u2B55")).to be(true)
+    end
+
+    it 'returns true if 4 of the same symbol in a column' do
+      subject.board[5][0] = "\u2B55"
+      subject.board[4][0] = "\u2B55"
+      subject.board[3][0] = "\u2B55"
+      subject.board[2][0] = "\u2B55"
+      expect(subject.connect_four?("\u2B55")).to be(true)
+    end
+
+    it 'returns true if 4 of the same symbol in a positive slope' do
+      subject.board[0][3] = "\u2B55"
+      subject.board[1][2] = "\u2B55"
+      subject.board[2][1] = "\u2B55"
+      subject.board[3][0] = "\u2B55"
+      expect(subject.connect_four?("\u2B55")).to be(true)
+    end
+
+    it 'returns true if 4 of the same symbol in a negative slope' do
+      subject.board[0][0] = "\u2B55"
+      subject.board[1][1] = "\u2B55"
+      subject.board[2][2] = "\u2B55"
+      subject.board[3][3] = "\u2B55"
+      expect(subject.connect_four?("\u2B55")).to be(true)
+    end
+  end
 end
 
 
